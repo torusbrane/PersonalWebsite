573b8854ff630f5a53dfc101330fcac6f1ba88d3
Tech Taurus <71295055+torusbrane@users.noreply.github.com>
2024-03-28 15:30:18 -0500

    attempt number 3 for forest7.jpg

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import './Home.css'; // Make sure the path is correct
 
 const Home = () => {
     return (
-        <div className="home-container">
+        <div className="home-container" style={{ backgroundImage: `url(${'/Forest7.jpg'})`}}>
             <div className="main-text">David Jones Portfolio</div>
         </div>
     );
